46125ece31d6d5a952f75184eeb2ac82c3de8da2
nfreireh <nfreireh@gmail.com>
2015-06-13 01:22:19 +0200

    Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,5 @@
 var models = require('../models/models.js');
 
-//GET /quizes/question
-/*exports.question = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question',{pregunta:quiz[0].pregunta})
-	})	
-};*/
-
 //Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req,res,next,quizId){
 	models.Quiz.find(quizId).then(
@@ -19,12 +12,25 @@ exports.load = function(req,res,next,quizId){
 		).catch(function(error){next(error);});
 };
 
-//GET /quizes
+//GET /quizes/ :search
 exports.index = function(req,res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs',{quizes:quizes});
-	}).catch(function(error){next(error);})
+	if(req.query.search){
+		var search = req.query.search.replace(' ','%');
+		models.Quiz.findAll({where:["pregunta like ?",'\%'+search+'\%']}).then(function(quizes){
+			res.render('quizes/index.ejs',{quizes:quizes});
+		}).catch(function(error){next(error);})
+	}else{
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs',{quizes:quizes});
+		}).catch(function(error){next(error);})
+	}
+
+
 };
+
+
+
+
 
 //GET /quizes/:id
 exports.show = function(req,res){
@@ -32,22 +38,10 @@ exports.show = function(req,res){
 };
 
 
-//GET /quizes/answer
-/*exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta: 'Correcto'});
-		}
-		else {
-			res.render('quizes/answer',{respuesta: 'Incorrecto'});
-		}
-	})
-};*/
-
 //GET /quizes/:id/answer
 exports.answer = function(req,res){
 	var resultado = 'Incorrecto';
-	if(req.query.respuesta === req.quiz.respuesta){
+	if(req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()){
 		resultado = 'Correcto';
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
